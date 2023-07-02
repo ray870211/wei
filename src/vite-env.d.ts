8252e7f95551857a036b6836ce20a1dee56cd6a8
= <=>
2023-07-03 02:21:14 +0800

    fix:調整env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,27 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pages/client" />
+/// <reference types="vite-svg-loader" />
+
+declare module 'virtual:vite-auto-plugins' {
+  import type { Plugin } from './main';
+  const plugins: Plugin[];
+  export default plugins;
+}
+
+declare module 'virtual:vite-auto-layouts' {
+  import type { Component } from 'vue';
+  const layouts: Record<string, Component>;
+  export default layouts;
+}
+
+declare module 'virtual:vite-auto-middleware' {
+  import type { RouteMiddleware } from './main';
+  const middlewareEntries: RouteMiddleware[];
+  export default middlewareEntries;
+}
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue';
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
